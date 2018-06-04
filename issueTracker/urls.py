0494ba15f0b7a77309e4c9cd2fd9dded23c77568
Rick <RickHarrisIT@gmx.com>
2018-06-04 13:47:56 +0100

    commit

--- issueTracker/urls.py
+++ issueTracker/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.urls import path
+from django.urls import include, path
 from django.contrib import admin
 admin.autodiscover()
 import hello.views
