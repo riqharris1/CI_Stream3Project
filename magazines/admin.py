e12c5112e1003fb2b667f5ab0f3b43e77a3e3a02
Rick <RickHarrisIT@gmx.com>
2018-06-04 12:05:22 +0100

    commit

--- magazines/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Magazine
-from .models import Purchase
-
-admin.site.register(Magazine)
-admin.site.register(Purchase)
--- threads/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Subject, Thread, Post
-
-admin.site.register(Subject)
-admin.site.register(Thread)
-admin.site.register(Post)
