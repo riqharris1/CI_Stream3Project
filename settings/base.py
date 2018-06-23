843b925fd6b43bd0a82911a96d49d2ad56df7411
Rick <RickHarrisIT@gmx.com>
2018-06-23 17:37:51 +0100

    forum

--- settings/base.py
+++ settings/base.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
-    'django.rest.framework',
+    'rest_framework',
     'tinymce',
     'emoticons',
     'disqus',
