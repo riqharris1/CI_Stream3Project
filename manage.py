4e3aef0e84b4209dd7aa82da96b09e977c5a39d2
Rick <RickHarrisIT@gmx.com>
2018-06-24 14:47:45 +0100

    forum

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-env/scripts/activate_this.py
+#env/scripts/activate_this.py
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.staging")
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
 
     from django.core.management import execute_from_command_line
 
