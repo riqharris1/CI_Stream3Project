b3aae764fc3bd994b5654fdd795c5ac8339ec681
Rick <RickHarrisIT@gmx.com>
2018-07-24 22:34:20 +0100

    database

--- _hello/models.py
+++ _hello/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+    when = models.DateTimeField(max_length=255, 'date created', auto_now_add=True)
