c03d25335532decb67e29308d96cc5bea36cfb48
Rick <RickHarrisIT@gmx.com>
2018-07-24 22:15:39 +0100

    database

--- magazines/models.py
+++ magazines/models.py
@@ -17,7 +17,7 @@ class Purchase(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='purchases',on_delete=models.CASCADE,)
     magazine = models.ForeignKey(Magazine,on_delete=models.CASCADE,)
-    subscription_end = models.DateTimeField(default=timezone.now)
+    subscription_end = models.CharField(default=timezone.now)
 
 
 # To avoid an "apps not loaded" error the import from signals is placed at the bottom of file
