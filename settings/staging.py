7619dfc86d913aa5114a55d434fa0ac8d30108ff
Rick <RickHarrisIT@gmx.com>
2018-08-18 20:07:58 +0100

    db.json

--- settings/staging.py
+++ settings/staging.py
@@ -10,11 +10,11 @@ DEBUG = False
 # Load the ClearDB connection details from the environment variable
 #DATABASES['default'] =  dj_database_url.config()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
         'default': dj_database_url.config('CLEARDB_DATABASE_URL'),
-    }
+    #}
 }    
     
 ''' 
