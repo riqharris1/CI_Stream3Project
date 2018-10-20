bfb574f44dec333c88f453edbe17371fd0af84e4
Rick <RickHarrisIT@gmx.com>
2018-10-20 11:41:10 +0100

    django_session

--- settings/staging.py
+++ settings/staging.py
@@ -14,7 +14,7 @@ DATABASES = {
         #'default': dj_database_url.config('DATABASE_URL'),
         'default': dj_database_url.config('CLEARDB_IVORY_URL'),
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR ,'C:\Users\Rick\Documents\Stream3Project\db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
         
     }
 }    
