e9d1553320466919dd639233667c94dd00f6c8a0
Rick <RickHarrisIT@gmx.com>
2018-10-21 10:13:09 +0100

    django_session

--- settings/staging.py
+++ settings/staging.py
@@ -11,8 +11,10 @@ DEBUG = True
 #DATABASES['default'] =  dj_database_url.config()
 DATABASES = {
     'default': {
-        'default': dj_database_url.config('CLEARDB_DATABASE_URL'),
+        #'default': dj_database_url.config('CLEARDB_DATABASE_URL'),
         #'default': dj_database_url.config('CLEARDB_IVORY_URL'),
+        'default': dj_database_url.config('CLEARDB_YELLOW_URL'),
+        
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
         
