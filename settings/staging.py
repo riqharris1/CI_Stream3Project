c89e0161149bea263f8a50762bf80f6663579cc0
Rick <RickHarrisIT@gmx.com>
2018-10-28 18:27:44 +0000

    django_session

--- settings/staging.py
+++ settings/staging.py
@@ -17,7 +17,8 @@ DATABASES = {
         'default': dj_database_url.config('CLEARDB_PURPLE_URL'),
         #'default': dj_database_url.config('DATABASE_URL'),
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR , 'CLEARDB_YELLOW_URL'),
+        #'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
         
     }
 }    
